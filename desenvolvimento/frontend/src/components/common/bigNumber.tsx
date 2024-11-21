1c089dcff1bb223d21e340857c9055f9c01ae623
Pedro Dias <pedrofelipe18@hotmail.com.br>
2024-11-20 21:06:56 -0300

    chore: add big number component

---
+++ desenvolvimento/frontend/src/components/common/bigNumber.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function BigNumber({
+  label,
+  value,
+}: {
+  label: string;
+  value: any;
+}) {
+  if (!value) return null;
+
+  return (
+    <div className="w-fit h-fit rounded-md bg-neutral-200 p-4 flex flex-col gap-2 justify-center items-center shadow-sm">
+      <span className="text-sm text-neutral-500">{label}</span>
+      <h1 className="text-2xl font-bold text-neutral-800">{value}</h1>
+    </div>
+  );
+}
