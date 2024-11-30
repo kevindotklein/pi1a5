0fff7e063d75d4fd97b9cefdb5505f4f8bd7ae50
Pedro Dias <pedrofelipe18@hotmail.com.br>
2024-11-30 18:52:08 -0300

    chore: implement steps

--- desenvolvimento/frontend/src/app/auth/register/page.tsx
+++ desenvolvimento/frontend/src/app/auth/register/page.tsx
@@ -24,6 +24,11 @@ import { useAction } from "@/hooks/useAction";
 import { collection, doc, setDoc } from "firebase/firestore";
 import { useLoading } from "@/contexts/loading";
 import { useTranslation } from "react-i18next";
+import { useState } from "react";
+import BaseStep from "./steps/baseStep";
+import { BookUser, PersonStanding, Shield } from "lucide-react";
+import SurveyStep from "./steps/surveyStep";
+import PrivacyStep from "./steps/privacyStep";
 
 export default function Register() {
   const { toast } = useToast();
@@ -32,20 +37,13 @@ export default function Register() {
 
   const { t, i18n } = useTranslation();
 
-  const formSchema = z.object({
-    full_name: z.string().min(3),
-    email: z.string().email(),
-    password: z.string(),
-  });
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-  });
+  const [userData, setUserData] = useState(null) as any;
+  const [step, setStep] = useState(0) as any;
 
   const router = useRouter();
 
-  const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    const { full_name, email, password } = values;
+  const onSubmit = async (data: any) => {
+    const { email, password } = data;
 
     setLoading(true);
     await action(
@@ -54,9 +52,10 @@ export default function Register() {
 
         const userId = auth.currentUser?.uid;
 
+        delete data.password;
+
         await setDoc(doc(firestore, "users", userId as string), {
-          full_name,
-          email,
+          ...data,
           has_notice: false,
           plan: "free",
         });
@@ -71,96 +70,67 @@ export default function Register() {
     );
   };
 
+  const steps = [
+    {
+      icon: <PersonStanding size={40} color="white" />,
+      title: "Seus dados",
+      description: "Preencha seus dados para continuar",
+      component: (
+        <BaseStep
+          setUserData={setUserData}
+          userData={userData}
+          setStep={setStep}
+        />
+      ),
+    },
+    {
+      icon: <BookUser size={40} color="white" />,
+      title: "Por que você está aqui?",
+      description: "Nos conte um pouco mais sobre seus estudos",
+      component: (
+        <SurveyStep
+          setUserData={setUserData}
+          userData={userData}
+          setStep={setStep}
+        />
+      ),
+    },
+    {
+      icon: <Shield size={40} color="white" />,
+      title: "Senha e Privacidade",
+      description: "Crie uma senha e aceite os termos",
+      component: (
+        <PrivacyStep
+          setUserData={setUserData}
+          userData={userData}
+          setStep={setStep}
+          submit={onSubmit}
+        />
+      ),
+    },
+  ];
+
+  const currentStep = steps[step];
+
   return (
-    <div className="h-full flex flex-col gap-4 tablet:w-full">
+    <div className="h-full flex flex-col gap-2 tablet:w-full">
+      <span className="text-sm text-white">
+        {step + 1} / {steps.length}
+      </span>
       <div className="h-full flex flex-col gap-4">
         <div className="flex flex-col gap-1">
-          <h3 className="text-3xl font-bold flex gap-2 items-center text-neutral-100">
-            studyflow
-          </h3>
-          <h2 className="text-sm text-neutral-300">{t("register.subtitle")}</h2>
-        </div>
-        <Form {...form}>
-          <div className="h-full">
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="w-full h-full flex flex-col justify-between gap-4"
-            >
-              <div className="flex flex-col gap-5">
-                <FormField
-                  control={form.control}
-                  name="full_name"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="!text-white">
-                        {t("register.name")}
-                      </FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder={t("register.name-input")}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="!text-white">
-                        {t("register.email")}
-                      </FormLabel>
-                      <FormControl>
-                        <Input
-                          placeholder={t("register.email-input")}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel className="!text-white">
-                        {t("register.password")}
-                      </FormLabel>
-                      <FormControl>
-                        <Input
-                          type="password"
-                          placeholder={t("register.password-input")}
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-
-              <div className="flex items-baseline justify-between mt-8 gap-2 tablet:!flex-col tablet:w-full">
-                <Button variant="link" asChild>
-                  <Link
-                    href="/auth/login"
-                    className="text-sm hover:underline tablet:!text-xs text-white tablet:w-full"
-                  >
-                    {t("register.login-question")}
-                  </Link>
-                </Button>
-
-                <Button variant="default" size="lg" className="tablet:w-full">
-                  {t("register.submit-button")}
-                </Button>
-              </div>
-            </form>
+          <div className="flex  gap-3 items-center">
+            {currentStep.icon}
+            <h3 className="text-3xl font-bold flex gap-2 items-center text-neutral-100">
+              {currentStep.title}
+            </h3>
           </div>
-        </Form>
+          <h2 className="text-sm text-neutral-300">
+            {currentStep.description}
+          </h2>
+        </div>
+
+        <div className="min-w-[400px]">{currentStep.component}</div>
       </div>
     </div>
   );
