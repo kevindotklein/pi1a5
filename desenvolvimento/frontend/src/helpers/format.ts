eabe934197d75e4bc45902f34b8c83f62f6394c7
Pedro Dias <pedrofelipe18@hotmail.com.br>
2024-11-30 17:23:40 -0300

    chore: add format helper

---
+++ desenvolvimento/frontend/src/helpers/format.ts
@@ -0,0 +1,24 @@
+export const cpfCnpjMask = (value: string) => {
+  if (!value) return "";
+
+  let result;
+
+  if (value.length < 14 || (value.length <= 14 && value.includes("."))) {
+    result = value
+      .replace(/\D/g, "")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+      .replace(/(-\d{2})\d+?$/, "$1");
+  } else {
+    result = value
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1/$2")
+      .replace(/(\d{4})(\d)/, "$1-$2")
+      .replace(/(-\d{2})\d+?$/, "$1");
+  }
+
+  return result;
+};
