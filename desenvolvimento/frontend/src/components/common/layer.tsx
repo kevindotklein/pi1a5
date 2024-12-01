cba94ba45af2b0214b84fdf50f74a5f54bbf4ea2
Pedro Dias <pedrofelipe18@hotmail.com.br>
2024-12-01 18:05:06 -0300

    chore: improve more styles

--- desenvolvimento/frontend/src/components/common/layer.tsx
+++ desenvolvimento/frontend/src/components/common/layer.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function Layer({ children, day, count }: Props) {
   return (
-    <div className="flex flex-col gap-1 min-w-[200px] bg-zinc-200 rounded-sm w-60 justify-start items-center py-3 px-3 tablet:h-full">
+    <div className="flex flex-col gap-1 min-w-[200px] bg-blue-200 rounded-sm w-60 justify-start items-center py-3 px-3 tablet:h-full">
       <div className="w-full flex items-end gap-2 mb-3">
         <p className="text-xl font-bold text-blue-800 select-none">{day}</p>
         <p className="text-lg font-bold text-blue-400 select-none">{count}</p>
